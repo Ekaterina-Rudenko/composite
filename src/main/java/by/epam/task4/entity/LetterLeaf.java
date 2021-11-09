58c4d9739550cda5633eb96e7e2ec078a3061722
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-09 10:53:52 +0300

    Create LetterLeaf.java

---
+++ src/main/java/by/epam/task4/entity/LetterLeaf.java
@@ -0,0 +1,36 @@
+package by.epam.task4.entity;
+
+import java.util.List;
+
+public class LetterLeaf implements TextComponent{
+    private char letter;
+    TextComponentType type;
+
+    public LetterLeaf(char letter, TextComponentType type){
+        this.letter = letter;
+        this.type = type;
+    }
+
+    @Override
+    public boolean add(TextComponent textComponent){
+        throw new UnsupportedOperationException("Can't add textComponent " + textComponent + " to letter");
+    }
+
+    @Override
+    public  boolean remove(TextComponent textComponent){
+        throw new UnsupportedOperationException("Can't remove " + textComponent + " from letter");
+    }
+    @Override
+    public List<TextComponent> getComponents(){
+        throw new UnsupportedOperationException("Letter is a terminal leaf");
+    }
+    @Override
+    public TextComponentType getType(){
+        return type;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(letter);
+    }
+}
