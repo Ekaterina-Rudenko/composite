2ca56e5d4f52a935e6145ffa681c67a79926f266
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-09 10:53:49 +0300

    Create PunctuationLeaf.java

---
+++ src/main/java/by/epam/task4/entity/PunctuationLeaf.java
@@ -0,0 +1,35 @@
+package by.epam.task4.entity;
+
+import java.util.List;
+
+public class PunctuationLeaf implements TextComponent {
+    private char punctuation;
+    TextComponentType type;
+
+    public PunctuationLeaf(char punctuation, TextComponentType type) {
+        this.punctuation = punctuation;
+        this.type = type;
+    }
+
+    @Override
+    public boolean add(TextComponent textComponent) {
+        throw new UnsupportedOperationException("Component " + textComponent + " can't be added to punctuation symbol");
+    }
+    @Override
+    public boolean remove(TextComponent textComponent) {
+        throw new UnsupportedOperationException("Component " + textComponent + " can't be removed from punctuation symbol");
+    }
+    @Override
+    public List<TextComponent> getComponents(){
+        throw  new UnsupportedOperationException("Punctuation leaf ia a terminal element");
+    }
+    @Override
+    public TextComponentType getType(){
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(punctuation);
+    }
+}
