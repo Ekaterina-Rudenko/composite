ea6b96c243717253d1470ba81e2d48d8b24a6d85
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-09 10:52:56 +0300

    Create Main.java

---
+++ src/main/java/by/epam/task4/main/Main.java
@@ -0,0 +1,48 @@
+package by.epam.task4.main;
+
+import by.epam.task4.entity.TextComponent;
+import by.epam.task4.entity.TextComponentType;
+import by.epam.task4.entity.TextComposite;
+import by.epam.task4.exception.CustomException;
+import by.epam.task4.handler.AbstractHandler;
+import by.epam.task4.handler.TextHandler;
+import by.epam.task4.reader.DataReader;
+import by.epam.task4.reader.impl.DataReaderImpl;
+import by.epam.task4.service.TextService;
+import by.epam.task4.service.impl.TextServiceImpl;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class Main {
+    static Logger logger = LogManager.getLogger();
+    static final String FILE_PATH = "data/data.txt";
+
+    public static void main(String[] args) {
+        DataReader reader = new DataReaderImpl();
+        String result;
+        try {
+            result = reader.readData(FILE_PATH);
+            System.out.println(result);
+            AbstractHandler handler = new TextHandler();
+            TextComponent component = new TextComposite(TextComponentType.TEXT);
+            handler.handleRequest(component, result);
+            logger.info("Parsed data:\n" + component);
+            TextService service = new TextServiceImpl();
+            List<TextComponent> componentList = service.sortParagraphsBySentence(component);
+            logger.info("Sorting by the number of sentences in the paragraph:\n" + componentList);
+            TextComponent sentenceWithLongestWord = service.findSentenceWithLongestWord(component);
+            logger.info("Sentence with the longest word:\n" + sentenceWithLongestWord);
+            List<TextComponent> suitableSentences = service.deleteSentencesUnderWordLimit(component, 18);
+            logger.info("Sentence with the number of words more than:\n" + suitableSentences);
+            long numberOfConsonant = service.countConsonant(component);
+            logger.info("Number of consonants in the text:" + numberOfConsonant);
+            long numberOfVowel = service.countVowel(component);
+            logger.info("Number of vowels in the text:" + numberOfVowel);
+        } catch (CustomException e) {
+            logger.log(Level.ERROR, e);
+        }
+    }
+}
