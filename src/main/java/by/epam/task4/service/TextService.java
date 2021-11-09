9cb5214fca5976db629ecab738d5a4c6f21473f6
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-09 10:52:42 +0300

    Create TextService.java

---
+++ src/main/java/by/epam/task4/service/TextService.java
@@ -0,0 +1,16 @@
+package by.epam.task4.service;
+
+import by.epam.task4.entity.TextComponent;
+import by.epam.task4.exception.CustomException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface TextService {
+    List<TextComponent> sortParagraphsBySentence(TextComponent text) throws CustomException;
+    TextComponent findSentenceWithLongestWord(TextComponent text) throws CustomException;
+    List<TextComponent> deleteSentencesUnderWordLimit(TextComponent text, int wordLimit) throws CustomException;
+    Map<String, Integer> findSameWords(TextComponent text);
+    long countConsonant(TextComponent text) throws CustomException;
+    long countVowel(TextComponent text) throws CustomException;
+}
