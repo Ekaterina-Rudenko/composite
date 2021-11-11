dc4eab55f4c78f22bd0ee42825f5a874787477f5
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-11 20:03:15 +0300

    Update LetterHandler.java

--- src/main/java/by/epam/task4/handler/LetterHandler.java
+++ src/main/java/by/epam/task4/handler/LetterHandler.java
@@ -3,7 +3,6 @@ package by.epam.task4.handler;
 import by.epam.task4.entity.LetterLeaf;
 import by.epam.task4.entity.TextComponent;
 import by.epam.task4.entity.TextComponentType;
-import by.epam.task4.entity.TextComposite;
 
 public class LetterHandler extends AbstractHandler{
     @Override
