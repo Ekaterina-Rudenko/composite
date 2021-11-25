e552bec1ded7c056789c25544db0ff053f42fb69
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-26 00:54:38 +0300

    Update TextComponent.java

--- src/main/java/by/epam/task4/entity/TextComponent.java
+++ src/main/java/by/epam/task4/entity/TextComponent.java
@@ -5,7 +5,12 @@ import java.util.List;
 public interface TextComponent {
 
     boolean add(TextComponent textComponent);
+
     boolean remove(TextComponent textComponent);
+
     List<TextComponent> getComponents();
+
+    TextComponent getComponentCopy();
+
     TextComponentType getType();
 }
