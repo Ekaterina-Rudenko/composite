9716ea7005a60137f6ab8865b95e2e0c25b38a77
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-26 00:55:08 +0300

    Update TextServiceImpl.java

--- src/main/java/by/epam/task4/service/impl/TextServiceImpl.java
+++ src/main/java/by/epam/task4/service/impl/TextServiceImpl.java
@@ -11,7 +11,6 @@ import org.apache.logging.log4j.Logger;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TextServiceImpl implements TextService {
     static final Logger logger = LogManager.getLogger();
@@ -20,93 +19,45 @@ public class TextServiceImpl implements TextService {
 
     @Override
     public List<TextComponent> sortParagraphsBySentence(TextComponent text) throws CustomException {
-        if (text == null | !text.getType().equals(TextComponentType.TEXT)) {
-            throw new CustomException("Incorrect type " + text.getType() + " of text component");
-        }
+        validateTextComponent(text);
         List<TextComponent> paragraphs = text.getComponents();
-
-        paragraphs.sort(new Comparator<TextComponent>() {
-            @Override
-            public int compare(TextComponent o1, TextComponent o2) {
-                Integer size1 = o1.getComponents().size();
-                Integer size2 = o2.getComponents().size();
-                return size1.compareTo(size2);
-            }
-        });
+        paragraphs.sort(Comparator.comparingInt(paragraph -> paragraph.getComponents().size()));
         return paragraphs;
     }
 
     @Override
-    public TextComponent findSentenceWithLongestWord(TextComponent text) throws CustomException {
-        if (text == null | !text.getType().equals(TextComponentType.TEXT)) {
-            throw new CustomException("Incorrect type " + text.getType() + " of text component");
-        }
-        List<TextComponent> sentences = text.getComponents().stream()
-                .flatMap(par -> par.getComponents().stream())
-                .collect(Collectors.toList());
-
-        List<TextComponent> words = sentences.stream()
-                .flatMap(sentence -> sentence.getComponents().stream()) //get lexemes
-                .flatMap(lexeme -> lexeme.getComponents().stream()) //get words or punctuation
-                .filter(word -> !(word instanceof PunctuationLeaf)) //exclude punctuation
-                .collect(Collectors.toList());
-
-        TextComponent longestWord = words.stream()
-                .max(new Comparator<TextComponent>() {
-                    @Override
-                    public int compare(TextComponent o1, TextComponent o2) {
-                        Integer size1 = o1.getComponents().size();
-                        Integer size2 = o2.getComponents().size();
-                        return size1.compareTo(size2);
-                    }
-                }).orElseThrow(() -> new CustomException("Stream of words is empty"));
+    public List<String> findSentenceWithLongestWord(TextComponent text) throws CustomException {
+        validateTextComponent(text);
+        TextComponent longestWord = getWords(text).stream()
+                .max(Comparator.comparingInt(word -> word.getComponents().size()))
+                .orElseThrow(() -> new CustomException("Stream of words is empty"));
 
         String longestWordString = longestWord.toString();
         logger.info("The longest word:\n" + longestWordString);
 
-        for (TextComponent sentence : sentences) {
-            String sentenceString = sentence.toString();
-            if (sentenceString.contains(longestWordString)) {
-                return sentence;
-            }
-        }
-        throw new CustomException("This exception will not be thrown");
+        List<String> resultSentence = text.getComponents().stream()
+                .flatMap(p -> p.getComponents().stream())
+                .map(s -> s.toString())
+                .filter(s -> s.contains(longestWordString))
+                .collect(Collectors.toList());
+        return resultSentence;
     }
 
     @Override
     public List<TextComponent> deleteSentencesUnderWordLimit(TextComponent text, int wordLimit) throws CustomException {
-        if (text == null | !text.getType().equals(TextComponentType.TEXT)) {
-            throw new CustomException("Incorrect type " + text.getType() + " of text component");
-        }
-        List<TextComponent> sentences = text.getComponents().stream()
-                .flatMap(par -> par.getComponents().stream())
+        validateTextComponent(text);
+        TextComponent textCopy = text.getComponentCopy();
+        List<TextComponent> correctSentences = textCopy.getComponents().stream()
+                .flatMap(paragraph -> paragraph.getComponents().stream()
+                        .filter(sentence -> countWords(sentence) > wordLimit))
                 .collect(Collectors.toList());
-
-        List<TextComponent> correctSentences = sentences.stream()
-                .filter(s -> countWords(s) > wordLimit)
-                .collect(Collectors.toList());
-
         return correctSentences;
-    }
-
-    private int countWords(TextComponent sentence) {
-        long numberOfWords = sentence.getComponents().stream()
-                .flatMap(l -> l.getComponents().stream())
-                .filter(w -> !(w instanceof PunctuationLeaf))
-                .count();
-        return (int) numberOfWords;
     }
 
     @Override
     public Map<String, Integer> findSameWords(TextComponent text) throws CustomException {
-        if (text == null | !text.getType().equals(TextComponentType.TEXT)) {
-            throw new CustomException("Incorrect type " + text.getType() + " of text component");
-        }
-        List<String> words = text.getComponents().stream()
-                .flatMap(p -> p.getComponents().stream())
-                .flatMap(s -> s.getComponents().stream())
-                .flatMap(l -> l.getComponents().stream())
-                .filter(w -> !(w instanceof PunctuationLeaf))
+        validateTextComponent(text);
+        List<String> words = getWords(text).stream()
                 .map(w -> w.toString().toLowerCase())
                 .collect(Collectors.toList());
         Map<String, Integer> sameWords = new HashMap<>();
@@ -131,10 +82,39 @@ public class TextServiceImpl implements TextService {
 
     @Override
     public long countConsonant(TextComponent text) throws CustomException {
-        if (text == null | !text.getType().equals(TextComponentType.TEXT)) {
-            throw new CustomException("Incorrect type " + text.getType() + " of text component");
-        }
+        validateTextComponent(text);
         Pattern pattern = Pattern.compile(CONSONANT_REGEX);
+        long consonantNumber = countLetters(text, pattern);
+        return consonantNumber;
+    }
+
+    @Override
+    public long countVowel(TextComponent text) throws CustomException {
+        validateTextComponent(text);
+        Pattern pattern = Pattern.compile(VOWEL_REGEX);
+        long vowelNumber = countLetters(text, pattern);
+        return vowelNumber;
+    }
+
+    private List<TextComponent> getWords(TextComponent text) {
+        List<TextComponent> words = text.getComponents().stream()
+                .flatMap(p -> p.getComponents().stream())
+                .flatMap(s -> s.getComponents().stream())
+                .flatMap(l -> l.getComponents().stream())
+                .filter(w -> !(w instanceof PunctuationLeaf))
+                .collect(Collectors.toList());
+        return words;
+    }
+
+    private int countWords(TextComponent sentence) {
+        long numberOfWords = sentence.getComponents().stream()
+                .flatMap(l -> l.getComponents().stream())
+                .filter(w -> !(w instanceof PunctuationLeaf))
+                .count();
+        return (int) numberOfWords;
+    }
+
+    private long countLetters(TextComponent text, Pattern pattern) {
         long counter = text.getComponents().stream()
                 .flatMap(p -> p.getComponents().stream())
                 .flatMap(s -> s.getComponents().stream())
@@ -147,21 +127,11 @@ public class TextServiceImpl implements TextService {
         return counter;
     }
 
-    @Override
-    public long countVowel(TextComponent text) throws CustomException {
-        if (text == null | !text.getType().equals(TextComponentType.TEXT)) {
-            throw new CustomException("Incorrect type " + text.getType() + " of text component");
+    private void validateTextComponent(TextComponent text) throws CustomException {
+        if (text == null) {
+            throw new CustomException("Null value of text component.");
+        } else if (!text.getType().equals(TextComponentType.TEXT)) {
+            throw new CustomException("Incorrect type of text component: " + text.getType() + ". TEXT type is expected.");
         }
-        Pattern pattern = Pattern.compile(VOWEL_REGEX);
-        long counter = text.getComponents().stream()
-                .flatMap(p -> p.getComponents().stream())
-                .flatMap(s -> s.getComponents().stream())
-                .flatMap(l -> l.getComponents().stream()
-                        .filter(w -> !(w instanceof PunctuationLeaf)))
-                .flatMap(w -> w.getComponents().stream())
-                .map(let -> let.toString())
-                .filter(let -> pattern.matcher(let).matches())
-                .count();
-        return counter;
     }
 }
