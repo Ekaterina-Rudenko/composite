985009c4373e830a5d3346ed7449172a1566c1d5
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-26 00:54:58 +0300

    Update TextHandler.java

--- src/main/java/by/epam/task4/handler/TextHandler.java
+++ src/main/java/by/epam/task4/handler/TextHandler.java
@@ -3,7 +3,7 @@ package by.epam.task4.handler;
 import by.epam.task4.entity.TextComponent;
 
 public class TextHandler extends AbstractHandler {
-    AbstractHandler successor = new ParagraphHandler();
+    private AbstractHandler successor = new ParagraphHandler();
     @Override
     public void handleRequest(TextComponent text, String data){
         successor.handleRequest(text, data);
